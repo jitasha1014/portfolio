bdffa6ab28f94257b750b40ea2c83a7090cdcdad
jitasha1014 <jitasha1014@gmail.com>
2025-02-21 17:06:37 +0530

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id='home' className='hero'>
+        <img src={profile_img} alt=''/>
+        <h1><span>I'm Jitasha Jain,</span> frontend developer based in India.</h1>
+        <p>I am a frontend developer from Rajasthan, India with self make project exprience.</p>
+        <div className="hero-action">
+            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='contact'>Connect with me</AnchorLink></div>
+            <div className="hero-resume">My resume</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
